5948690e7af7893955ee880f37b703e2e273adad
Andre Fahey <faheyandre@yahoo.co.uk>
2020-02-12 17:40:38 +0000

    Staff Authentication Started

---
+++ Date-Control-Application/src/app/staff-email/staff-email.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StaffEmailComponent } from './staff-email.component';
+
+describe('StaffEmailComponent', () => {
+  let component: StaffEmailComponent;
+  let fixture: ComponentFixture<StaffEmailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StaffEmailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StaffEmailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Date-Control-Application/src/app/staff-email/staff-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-email',
+  templateUrl: './staff-email.component.html',
+  styleUrls: ['./staff-email.component.css']
+})
+export class StaffEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
