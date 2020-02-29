e851a732aa6d0be18412e7ef1c4b96d7f3781b5f
Andre Fahey <faheyandre@yahoo.co.uk>
2020-02-29 15:28:08 +0000

    Added Firebase Details

--- Date-Control-Application/src/app/app.module.ts
+++ Date-Control-Application/src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+var config = {
+  apiKey: "AIzaSyDECLa7VgYNbXmrwVub8pDZquaICYYFleI",
+    authDomain: "datecontroldatabase.firebaseapp.com",
+    databaseURL: "https://datecontroldatabase.firebaseio.com",
+    projectId: "datecontroldatabase",
+    storageBucket: "datecontroldatabase.appspot.com",
+    messagingSenderId: "471452525084",
+    appId: "1:471452525084:web:a3e0d8c3e823462c7ab5d9",
+    measurementId: "G-8CV5FPTNWJ"
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
