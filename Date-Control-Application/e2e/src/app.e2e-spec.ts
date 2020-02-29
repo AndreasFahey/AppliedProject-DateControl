603963dc8718f6ab015cb68bb79d317a19808757
Andre Fahey <faheyandre@yahoo.co.uk>
2020-02-29 14:59:28 +0000

    Update

--- Date-Control-Application/e2e/src/app.e2e-spec.ts
+++ Date-Control-Application/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Date-Control-Application!');
+    expect(page.getTitleText()).toEqual('Date-Control-Application app is running!');
   });
 
   afterEach(async () => {
--- Date-Control-Application/src/app/app.module.ts
+++ Date-Control-Application/src/app/app.module.ts
@@ -1,40 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StaffLoginComponent } from './staff-login/staff-login.component';
-import { StaffRegistrationComponent } from './staff-registration/staff-registration.component';
-import { StaffEmailComponent } from './staff-email/staff-email.component';
-import { StaffMembersComponent } from './staff-members/staff-members.component';
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyDECLa7VgYNbXmrwVub8pDZquaICYYFleI",
-    authDomain: "datecontroldatabase.firebaseapp.com",
-    databaseURL: "https://datecontroldatabase.firebaseio.com",
-    projectId: "datecontroldatabase",
-    storageBucket: "datecontroldatabase.appspot.com",
-    messagingSenderId: "471452525084",
-    appId: "1:471452525084:web:a3e0d8c3e823462c7ab5d9",
-    measurementId: "G-8CV5FPTNWJ"
-}
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StaffLoginComponent,
-    StaffRegistrationComponent,
-    StaffEmailComponent,
-    StaffMembersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
