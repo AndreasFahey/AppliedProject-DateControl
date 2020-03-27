ad627f3dff713fbd3c4377d1377dab572d2f3ebd
Andre Fahey <faheyandre@yahoo.co.uk>
2020-03-27 17:16:19 +0000

    biscuits-cakes backend for crud done

--- Date-Control-Application/src/app/shared/biscuits-cakes.service.ts
+++ Date-Control-Application/src/app/shared/biscuits-cakes.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { BiscuitsCakes } from '../shared/BiscuitsCakes';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BiscuitsCakesService {
+bcListRef: AngularFireList<any>;
+bcRef: AngularFireObject<any>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  // Enter Product Deatils
+  enterBcDetails(bc: BiscuitsCakes) {
+    return this.bcListRef.push({
+      brand: bc.brand,
+      description: bc.description,
+      barcode: bc.barcode,
+      quantity: bc.quantity,
+      bbdate: bc.bbdate
+    })
+  }
+
+  // Get Single Details
+  getBcDetails(id: string){
+    this.bcRef = this.db.object('/biscuits-cakes/' + id);
+    return this.bcRef;
+  }
+
+  // Get Listed Details
+  getBcDetailsList(){
+    this.bcListRef = this.db.list('/biscuits-cakes');
+    return this.bcListRef;
+  }
+
+  // Update Deatils - If you delete some etc
+  updateBcDetails(id, bc:BiscuitsCakes){
+    return this.bcRef.update({
+      brand: bc.brand,
+      description: bc.description,
+      barcode: bc.barcode,
+      quantity: bc.quantity,
+      bbdate: bc.bbdate
+    })
+  }
+
+  // Delete Out of Date Products
+  deleteBcProduct(id: string){
+    this.bcRef = this.db.object('/biscuits-cakes/' + id);
+    this.bcRef.remove();
+  }
+
+
 }
