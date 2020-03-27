402603fa2a4731346a674162d36a9ef79de4d0c2
Andre Fahey <faheyandre@yahoo.co.uk>
2020-03-27 16:39:14 +0000

    started crud functionality

--- Date-Control-Application/src/environments/environment.prod.ts
+++ Date-Control-Application/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDECLa7VgYNbXmrwVub8pDZquaICYYFleI",
+    authDomain: "datecontroldatabase.firebaseapp.com",
+    databaseURL: "https://datecontroldatabase.firebaseio.com",
+    projectId: "datecontroldatabase",
+    storageBucket: "datecontroldatabase.appspot.com",
+    messagingSenderId: "471452525084",
+    appId: "1:471452525084:web:a3e0d8c3e823462c7ab5d9",
+    measurementId: "G-8CV5FPTNWJ"
+  }
 };
