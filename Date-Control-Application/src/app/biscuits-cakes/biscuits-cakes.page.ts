f28fd98dfa7be15ee8b82d946cf3977afc00854f
Andre Fahey <faheyandre@yahoo.co.uk>
2020-03-30 17:29:34 +0100

    Form working and product showing as list ! adding update and delete now

--- Date-Control-Application/src/app/biscuits-cakes/biscuits-cakes.page.ts
+++ Date-Control-Application/src/app/biscuits-cakes/biscuits-cakes.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder } from "@angular/forms";
 import { BiscuitsCakesService } from './../shared/biscuits-cakes.service';
+import { BiscuitsCakes } from '../shared/BiscuitsCakes';
 
 @Component({
   selector: 'app-biscuits-cakes',
@@ -10,6 +11,7 @@ import { BiscuitsCakesService } from './../shared/biscuits-cakes.service';
 })
 export class BiscuitsCakesPage implements OnInit {
   bcForm: FormGroup;
+  Listings = [];
 
   constructor(
     private bcService: BiscuitsCakesService,
@@ -25,18 +27,41 @@ export class BiscuitsCakesPage implements OnInit {
       quantity: [''],
       bbdate: ['']
     })
+    this.fetchBc();
+    let productInfo = this.bcService.getBcDetailsList();
+    productInfo.snapshotChanges().subscribe(res => {
+      this.Listings = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Listings.push(a as BiscuitsCakes);
+      })
+    })
   }
 
-  formsSubmit(){
+  formSubmit(){
     if (!this.bcForm.valid) {
       return false;
     } else {
       this.bcService.enterBcDetails(this.bcForm.value).then(res => {
         console.log(res)
         this.bcForm.reset();
-        //this.router.navigate(['/biscuits-cakes']);
+        this.router.navigate(['/biscuits-cakes']);
       })
         .catch(error => console.log(error));
+    }
+  }
+
+  fetchBc() {
+    this.bcService.getBcDetailsList().valueChanges().subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  deleteBcProduct(id) {
+    console.log(id)
+    if (window.confirm('Confirm Product Removal from Store Inventory?')) {
+      this.bcService.deleteBcProduct(id)
     }
   }
 }
