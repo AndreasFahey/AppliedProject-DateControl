8c7bcbe331d21865c99f751a496d6f3eb4323968
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-04 13:52:50 +0100

    frozen service backend ready for crud functionality on page

--- Date-Control-Application/src/app/shared/Frozen/frozen.service.ts
+++ Date-Control-Application/src/app/shared/Frozen/frozen.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Frozen } from './Frozen';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FrozenService {
+fzListRef: AngularFireList<any>;
+fzRef: AngularFireObject<any>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  // Enter Product Details
+  enterFzDetails(fz: Frozen) {
+    return this.fzListRef.push({
+      brand: fz.brand,
+      description: fz.description,
+      barcode: fz.barcode,
+      quantity: fz.quantity,
+      bbdate: fz.bbdate
+    })
+  }
+
+  // Get Single Details
+  getFzDetails(id: string){
+    this.fzRef = this.db.object('/frozen/' + id);
+    return this.fzRef;
+  }
+
+  // Get Listed Details
+  getFzDetailsList(){
+    this.fzListRef = this.db.list('/frozen');
+    return this.fzListRef;
+  }
+
+  // Update Details - If you delete some etc
+  updateFzDetails(id, fz:Frozen){
+    return this.fzRef.update({
+      brand: fz.brand,
+      description: fz.description,
+      barcode: fz.barcode,
+      quantity: fz.quantity,
+      bbdate: fz.bbdate
+    })
+  }
+
+  // Delete Out of Date Products
+  deleteFzProduct(id: string){
+    this.fzRef = this.db.object('/frozen/' + id);
+    this.fzRef.remove();
+  }
 }
