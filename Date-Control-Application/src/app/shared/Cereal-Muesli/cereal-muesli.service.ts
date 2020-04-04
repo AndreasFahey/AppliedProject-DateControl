2fc33e16979b5b9805247c9ea83113763f611fa7
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-04 13:12:42 +0100

    cereal-muesli service backend ready for crud functionality on page

--- Date-Control-Application/src/app/shared/Cereal-Muesli/cereal-muesli.service.ts
+++ Date-Control-Application/src/app/shared/Cereal-Muesli/cereal-muesli.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { CerealMuesli } from './CerealMuesli';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CerealMuesliService {
+cmListRef: AngularFireList<any>;
+cmRef: AngularFireObject<any>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  // Enter Product Deatils
+  enterCmDetails(cm: CerealMuesli) {
+    return this.cmListRef.push({
+      brand: cm.brand,
+      description: cm.description,
+      barcode: cm.barcode,
+      quantity: cm.quantity,
+      bbdate: cm.bbdate
+    })
+  }
+
+  // Get Single Details
+  getCmDetails(id: string){
+    this.cmRef = this.db.object('/cereal-muesli/' + id);
+    return this.cmRef;
+  }
+
+  // Get Listed Details
+  getCmDetailsList(){
+    this.cmListRef = this.db.list('/cereal-muesli');
+    return this.cmListRef;
+  }
+
+  // Update Deatils - If you delete some etc
+  updateCmDetails(id, cm:CerealMuesli){
+    return this.cmRef.update({
+      brand: cm.brand,
+      description: cm.description,
+      barcode: cm.barcode,
+      quantity: cm.quantity,
+      bbdate: cm.bbdate
+    })
+  }
+
+  // Delete Out of Date Products
+  deleteCmProduct(id: string){
+    this.cmRef = this.db.object('/cereal-muesli/' + id);
+    this.cmRef.remove();
+  }
+
 }
