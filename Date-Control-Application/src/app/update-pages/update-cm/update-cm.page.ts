90e16680af9a9dcd40e6909faae15c3f6c6ae1bc
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-17 19:50:45 +0100

    update/edit cereal muesli product done

--- Date-Control-Application/src/app/update-pages/update-cm/update-cm.page.ts
+++ Date-Control-Application/src/app/update-pages/update-cm/update-cm.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { CerealMuesliService } from '../../shared/Cereal-Muesli/cereal-muesli.service';
 
 @Component({
   selector: 'app-update-cm',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateCmPage implements OnInit {
 
-  constructor() { }
+  updateCmForm: FormGroup;
+  id: any;
+
+  constructor(
+    private cmService: CerealMuesliService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.cmService.getCmDetails(this.id).valueChanges().subscribe(res => {
+      this.updateCmForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateCmForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateCmForm.value)
+  }
+
+  updateForm() {
+    this.cmService.updateCmDetails(this.id, this.updateCmForm.value)
+      .then(() => {
+        this.router.navigate(['/cereal-muesli']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
