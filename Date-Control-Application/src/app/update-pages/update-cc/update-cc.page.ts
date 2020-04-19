aefdde7ccd6cf4ad1d278a04a40fddecf18e84e2
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-19 13:01:29 +0100

    update cc details, done

--- Date-Control-Application/src/app/update-pages/update-cc/update-cc.page.ts
+++ Date-Control-Application/src/app/update-pages/update-cc/update-cc.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { ConfectioneryCrispsService } from '../../shared/Confectionery-Crisps/confectionery-crisps.service';
 
 @Component({
   selector: 'app-update-cc',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateCcPage implements OnInit {
 
-  constructor() { }
+  updateCcForm: FormGroup;
+  id: any;
+
+  constructor(
+    private ccService: ConfectioneryCrispsService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.ccService.getCcDetails(this.id).valueChanges().subscribe(res => {
+      this.updateCcForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateCcForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateCcForm.value)
+  }
+
+  updateForm() {
+    this.ccService.updateCcDetails(this.id, this.updateCcForm.value)
+      .then(() => {
+        this.router.navigate(['/confectionery-crisps']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
