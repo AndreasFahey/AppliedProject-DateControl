26644535340cc4a9e32c54f6a897e41733a3d27b
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-24 12:37:38 +0100

    update deli stock product done

--- Date-Control-Application/src/app/update-pages/update-ds/update-ds.page.ts
+++ Date-Control-Application/src/app/update-pages/update-ds/update-ds.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { DeliService } from '../../shared/Deli-Stock/deli.service';
 
 @Component({
   selector: 'app-update-ds',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateDsPage implements OnInit {
 
-  constructor() { }
+  updateDsForm: FormGroup;
+  id: any;
+
+  constructor(
+    private dsService: DeliService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.dsService.getDsDetails(this.id).valueChanges().subscribe(res => {
+      this.updateDsForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateDsForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      batchcode:[''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateDsForm.value)
+  }
+
+  updateForm() {
+    this.dsService.updateDsDetails(this.id, this.updateDsForm.value)
+      .then(() => {
+        this.router.navigate(['/deli-stock']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
