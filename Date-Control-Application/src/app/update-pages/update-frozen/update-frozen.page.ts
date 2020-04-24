50fd67b7811aec9e0343ada279e02276febb97fa
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-24 13:00:16 +0100

    update frozen product details page done

--- Date-Control-Application/src/app/update-pages/update-frozen/update-frozen.page.ts
+++ Date-Control-Application/src/app/update-pages/update-frozen/update-frozen.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { FrozenService } from '../../shared/Frozen/frozen.service';
 
 @Component({
   selector: 'app-update-frozen',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateFrozenPage implements OnInit {
 
-  constructor() { }
+  updateFzForm: FormGroup;
+  id: any;
+
+  constructor(
+    private fzService: FrozenService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.fzService.getFzDetails(this.id).valueChanges().subscribe(res => {
+      this.updateFzForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateFzForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateFzForm.value)
+  }
+
+  updateForm() {
+    this.fzService.updateFzDetails(this.id, this.updateFzForm.value)
+      .then(() => {
+        this.router.navigate(['/frozen']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
