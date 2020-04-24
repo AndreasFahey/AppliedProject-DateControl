6394f3a6466c52f5f7c2478d1a21691878e2e494
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-24 12:17:56 +0100

    update dairy product page done

--- Date-Control-Application/src/app/update-pages/update-dairy/update-dairy.page.ts
+++ Date-Control-Application/src/app/update-pages/update-dairy/update-dairy.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { DairyService } from '../../shared/Dairy/dairy.service';
+
 
 @Component({
   selector: 'app-update-dairy',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateDairyPage implements OnInit {
 
-  constructor() { }
+  updateDpForm: FormGroup;
+  id: any;
+
+  constructor(
+    private dpService: DairyService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.dpService.getDpDetails(this.id).valueChanges().subscribe(res => {
+      this.updateDpForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateDpForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateDpForm.value)
+  }
+
+  updateForm() {
+    this.dpService.updateDpDetails(this.id, this.updateDpForm.value)
+      .then(() => {
+        this.router.navigate(['/dairy']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
