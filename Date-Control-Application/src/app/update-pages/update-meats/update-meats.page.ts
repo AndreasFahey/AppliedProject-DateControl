374850d090e6dd6f7740501ec2e96a5ae75c5b06
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-25 18:28:03 +0100

    update meat product page done

--- Date-Control-Application/src/app/update-pages/update-meats/update-meats.page.ts
+++ Date-Control-Application/src/app/update-pages/update-meats/update-meats.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { MeatsService } from '../../shared/Meats/meats.service';
 
 @Component({
   selector: 'app-update-meats',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateMeatsPage implements OnInit {
 
-  constructor() { }
+  updateMeForm: FormGroup;
+  id: any;
+
+  constructor(
+    private meService: MeatsService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.meService.getMeDetails(this.id).valueChanges().subscribe(res => {
+      this.updateMeForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateMeForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateMeForm.value)
+  }
+
+  updateForm() {
+    this.meService.updateMeDetails(this.id, this.updateMeForm.value)
+      .then(() => {
+        this.router.navigate(['/meats']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
