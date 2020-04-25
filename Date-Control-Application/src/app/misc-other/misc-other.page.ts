82d7359d92afd3fb5d63b537eccd6a891ce3ad5c
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-25 19:03:36 +0100

    misc page done

--- Date-Control-Application/src/app/misc-other/misc-other.page.ts
+++ Date-Control-Application/src/app/misc-other/misc-other.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { MiscService } from '../shared/Misc-Other/misc.service';
+import { Misc } from '../shared/Misc-Other/Misc';
 
 @Component({
   selector: 'app-misc-other',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiscOtherPage implements OnInit {
 
-  constructor() { }
+  miscForm: FormGroup;
+  Listings = [];
+
+  constructor(
+    private miscService: MiscService,
+    private router: Router,
+    public fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.miscForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode: [''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    this.fetchMisc();
+    let productInfo = this.miscService.getMiscDetailsList();
+    productInfo.snapshotChanges().subscribe(res => {
+      this.Listings = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Listings.push(a as Misc);
+      })
+    })
+  }
+
+  formSubmit(){
+    if (!this.miscForm.valid) {
+      return false;
+    } else {
+      this.miscService.enterMiscDetails(this.miscForm.value).then(res => {
+        console.log(res)
+        this.miscForm.reset();
+        this.router.navigate(['/misc-other']);
+      })
+        .catch(error => console.log(error));
+    }
+  }
+
+  fetchMisc() {
+    this.miscService.getMiscDetailsList().valueChanges().subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  deleteMiscProduct(id) {
+    console.log(id)
+    if (window.confirm('Confirm Product Removal from Store Inventory?')) {
+      this.miscService.deleteMiscProduct(id)
+    }
   }
 
 }
