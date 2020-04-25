1e31fa201cbf7ddd4a2a17d17df9c31ed9f4c1bd
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-25 18:53:20 +0100

    update minerals details done, small fix on update meds

--- Date-Control-Application/src/app/update-pages/update-minerals/update-minerals.page.ts
+++ Date-Control-Application/src/app/update-pages/update-minerals/update-minerals.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { MineralsService } from '../../shared/Minerals/minerals.service';
 
 @Component({
   selector: 'app-update-minerals',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateMineralsPage implements OnInit {
 
-  constructor() { }
+  updateMinForm: FormGroup;
+  id: any;
+
+  constructor(
+    private minService: MineralsService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.minService.getMinDetails(this.id).valueChanges().subscribe(res => {
+      this.updateMinForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateMinForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateMinForm.value)
+  }
+
+  updateForm() {
+    this.minService.updateMinDetails(this.id, this.updateMinForm.value)
+      .then(() => {
+        this.router.navigate(['/minerals']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
