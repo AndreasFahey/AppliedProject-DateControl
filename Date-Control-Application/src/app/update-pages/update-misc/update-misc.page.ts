7f682669503d912481679c28f49457b39c624321
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-25 19:07:23 +0100

    update misc product page done

--- Date-Control-Application/src/app/update-pages/update-misc/update-misc.page.ts
+++ Date-Control-Application/src/app/update-pages/update-misc/update-misc.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { MiscService } from '../../shared/Misc-Other/misc.service';
 
 @Component({
   selector: 'app-update-misc',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateMiscPage implements OnInit {
 
-  constructor() { }
+  updateMiscForm: FormGroup;
+  id: any;
+
+  constructor(
+    private miscService: MiscService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.miscService.getMiscDetails(this.id).valueChanges().subscribe(res => {
+      this.updateMiscForm.setValue(res);
+    });
+  }
 
   ngOnInit() {
+    this.updateMiscForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode:[''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    console.log(this.updateMiscForm.value)
+  }
+
+  updateForm() {
+    this.miscService.updateMiscDetails(this.id, this.updateMiscForm.value)
+      .then(() => {
+        this.router.navigate(['/misc-other']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
