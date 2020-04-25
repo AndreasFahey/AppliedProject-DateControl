643dff451a8c9f6b629e293623508fa073f13d6e
Andre Fahey <faheyandre@yahoo.co.uk>
2020-04-25 18:20:35 +0100

    meats page

--- Date-Control-Application/src/app/meats/meats.page.ts
+++ Date-Control-Application/src/app/meats/meats.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { MeatsService } from '../shared/Meats/meats.service';
+import { Meats } from '../shared/Meats/Meats';
 
 @Component({
   selector: 'app-meats',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeatsPage implements OnInit {
 
-  constructor() { }
+  meForm: FormGroup;
+  Listings = [];
+
+  constructor(
+    private meService: MeatsService,
+    private router: Router,
+    public fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.meForm = this.fb.group({
+      brand: [''],
+      description: [''],
+      barcode: [''],
+      quantity: [''],
+      bbdate: ['']
+    })
+    this.fetchMe();
+    let productInfo = this.meService.getMeDetailsList();
+    productInfo.snapshotChanges().subscribe(res => {
+      this.Listings = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Listings.push(a as Meats);
+      })
+    })
+  }
+
+  formSubmit(){
+    if (!this.meForm.valid) {
+      return false;
+    } else {
+      this.meService.enterMeDetails(this.meForm.value).then(res => {
+        console.log(res)
+        this.meForm.reset();
+        this.router.navigate(['/meats']);
+      })
+        .catch(error => console.log(error));
+    }
+  }
+
+  fetchMe() {
+    this.meService.getMeDetailsList().valueChanges().subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  deleteMeProduct(id) {
+    console.log(id)
+    if (window.confirm('Confirm Product Removal from Store Inventory?')) {
+      this.meService.deleteMeProduct(id)
+    }
   }
 
 }
